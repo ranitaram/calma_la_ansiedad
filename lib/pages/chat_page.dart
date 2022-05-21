636e094e4b794e28633827ece87d968d34db03d2
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-05-21 12:30:58 -0600

    adding topbar

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -34,10 +34,46 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceHeight * 0.03,
+            vertical: _deviceHeight * 0.02,
+          ),
+          height: _deviceHeight,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                this.widget.chat.title(),
+                fontSize: 25,
+                primaryAction: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Color.fromRGBO(0, 82, 218, 1.0),
+                    )),
+                secondaryAction: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Color.fromRGBO(0, 82, 218, 1.0),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
