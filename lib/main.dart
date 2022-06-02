e82e47a22d943756d784975a3a7b83700ee425f6
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-06-02 17:41:39 -0600

    agregado el metodo para inicializar las notificaciones desde consola firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:calmar_la_ansiedad/routes/routes.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 //Packages
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:io' show Platform;
 
 //Services
 import './services/navigation_service.dart';
@@ -13,11 +16,10 @@ import './providers/authentication_provider.dart';
 
 //Pages
 import './pages/splash_page.dart';
-import './pages/login_page.dart';
-import './pages/register_page.dart';
-import './pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     SplashPage(
       key: UniqueKey(),
@@ -30,7 +32,29 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    if (Platform.isIOS) {
+      final settings = await FirebaseMessaging.instance.requestPermission();
+      if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+        FirebaseMessaging.instance.subscribeToTopic('promos');
+      }
+    } else {
+      FirebaseMessaging.instance.subscribeToTopic('promos');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
