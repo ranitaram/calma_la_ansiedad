93ecf334c433c0643769d5aa17943f5fded5e577
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-06-06 12:34:50 -0600

    agregado el sdk de admob

--- android/app/src/main/kotlin/com/example/calmar_la_ansiedad/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calmar_la_ansiedad/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.calmar_la_ansiedad
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity: FlutterActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ...
+        MobileAds.initialize(this)
+    }
 }
