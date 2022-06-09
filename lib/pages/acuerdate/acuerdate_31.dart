9393b0b48db4757e3a956f2ddc7ae33435f2a1af
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-06-08 21:27:53 -0600

    agregado anuncios deÃl 27 al 32

--- lib/pages/acuerdate/acuerdate_31.dart
+++ lib/pages/acuerdate/acuerdate_31.dart
@@ -1,11 +1,12 @@
 import 'package:calmar_la_ansiedad/pages/descubrimientos/descubrimiento_31.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import '../../widgets/boton_gordo.dart';
 import '../../widgets/custom_parrafo.dart';
 
-import '../../widgets/parrafo_grande.dart';
+import '../../widgets/interstitial_clase_admob.dart';
 import '../../widgets/top_bar.dart';
 
 class Acuerdate31 extends StatefulWidget {
@@ -16,6 +17,25 @@ class Acuerdate31 extends StatefulWidget {
 class _Acuerdate31State extends State<Acuerdate31> {
   late double _deviceHeight;
   late double _deviceWidth;
+
+  AnunciosInterstitial _anunciosInterstitial = new AnunciosInterstitial();
+
+  @override
+  void initState() {
+    MobileAds.instance.initialize();
+    AnunciosInterstitial();
+    _anunciosInterstitial.createInter();
+    _anunciosInterstitial.showInter();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _anunciosInterstitial.createInter();
+    _anunciosInterstitial.showInter();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,6 +88,8 @@ class _Acuerdate31State extends State<Acuerdate31> {
                 color1: const Color.fromARGB(255, 34, 210, 183),
                 color2: const Color.fromARGB(255, 12, 85, 52),
                 onpress: () {
+                  _anunciosInterstitial.createInter();
+                  _anunciosInterstitial.showInter();
                   Navigator.push(
                       context,
                       MaterialPageRoute(
--- lib/pages/acuerdate/acuerdate_32.dart
+++ lib/pages/acuerdate/acuerdate_32.dart
@@ -1,10 +1,12 @@
 import 'package:calmar_la_ansiedad/pages/descubrimientos/descubrimiento_32.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import '../../widgets/boton_gordo.dart';
 import '../../widgets/custom_parrafo.dart';
 
+import '../../widgets/interstitial_clase_admob.dart';
 import '../../widgets/parrafo_grande.dart';
 import '../../widgets/top_bar.dart';
 
@@ -16,6 +18,25 @@ class Acuerdate32 extends StatefulWidget {
 class _Acuerdate32State extends State<Acuerdate32> {
   late double _deviceHeight;
   late double _deviceWidth;
+
+  AnunciosInterstitial _anunciosInterstitial = new AnunciosInterstitial();
+
+  @override
+  void initState() {
+    MobileAds.instance.initialize();
+    AnunciosInterstitial();
+    _anunciosInterstitial.createInter();
+    _anunciosInterstitial.showInter();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _anunciosInterstitial.createInter();
+    _anunciosInterstitial.showInter();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +91,8 @@ class _Acuerdate32State extends State<Acuerdate32> {
                 color1: const Color.fromARGB(255, 34, 210, 183),
                 color2: const Color.fromARGB(255, 12, 85, 52),
                 onpress: () {
+                  _anunciosInterstitial.createInter();
+                  _anunciosInterstitial.showInter();
                   Navigator.push(
                       context,
                       MaterialPageRoute(
